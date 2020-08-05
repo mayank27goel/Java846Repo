515c497e8f356cfabe1f95bf1c8741b7b98c131d
mayank27goel <65491051+mayank27goel@users.noreply.github.com>
2020-08-05 15:03:59 +0530

    product function added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -6,4 +6,8 @@ class Calculator{
   public void diff(int a,int b){
     System.out.println("Difference= "+(a-b));
   }
+  
+  public void prod(int a,int b){
+    System.out.println("Product= "+(a*b));
+  }
 }
